76b07894c751cb4efa012496151213203f94bf83
Liam McKenna <liam.mckenna@outlook.ie>
2024-01-29 21:15:08 +0000

    update job times

--- src/components/sections/Experience.js
+++ src/components/sections/Experience.js
@@ -2,7 +2,6 @@
 
 import React, { useState, useEffect } from "react";
 import styles from "./experience.module.scss";
-import Card from "../card/Card";
 import Accordion from "../accordion/Accordion";
 
 export default function Experience() {
@@ -35,49 +34,49 @@ export default function Experience() {
     {
       title: "Software Engineer",
       company: "Nuritas",
-      time: "(1Y 8M) Oct 2021 - Nov 2023",
+      time: "(2yrs) Oct 2021 - Oct 2023",
       description:
         "Developed and maintained an internal data warehouse and user interface Collaborated with Data Science teams to build APIs and logic functions Championed UI/UX design, improving and implementing new designs into React",
     },
     {
       title: "Web Developer",
       company: "Freelance",
-      time: "(5M) May 2021 - Oct 2021",
+      time: "(6mos) May 2021 - Oct 2021",
       description:
         "Working directly with Clients to design and develope full stack website aplications. Focus on the MERN stack with React frontend and Node/Express backend. Data bases focused on SQL or MongoDB. Designwork in photoshop and Figma",
     },
     {
       title: "Web Support Intern",
       company: "Glowfox",
-      time: "(5M) May 2021 - Oct 2021",
+      time: "(7mos) Aug 2020 - Feb 2021",
       description:
         "Worked Closely with B2B the integration of the Glofox Web plaform into the clients dedicated websites. First line of support of all web Developement quieres both internally and externally.",
     },
     {
       title: "Software Developer Intern",
       company: "Cloudtech Ltd",
-      time: "(5M) May 2021 - Oct 2021",
+      time: "(8mos) Jan 2020 - Aug 2020",
       description:
         "Custom Software Solutions. CRM Setup & Management. Native & Web development. Project Management & Documentation.",
     },
     {
       title: "1st & 2nd Tech Support",
       company: "Hewlett Packard",
-      time: "(5M) May 2021 -> Oct 2021",
+      time: "(1yr 8mos) Jun 2018 -> Jan 2020",
       description:
         "Specialist IT support to first level agents. resolving major issues with clients software and hardware. Active Directory provisioning. Coaching & mentorship of peers. Control of Knowledge Base.",
     },
     {
       title: "Audio Visual Technician",
       company: "Sonics AVI",
-      time: "(5M) May 2021 - Oct 2021",
+      time: "(5mos) Aug 2018 - Jun 2018",
       description:
         "Provide IT support to engineers. First line of IT support to client. Building comms racks. Hardware install & setup. face to face client support. Cable crimping & running.",
     },
     {
       title: "Graphic Designer & Media Manager",
       company: "Sin Nightclub",
-      time: "(5Y 3M) May 2021 - Oct 2021",
+      time: "(5yrs 4mos) May 2012 - Aug 2017",
       description:
         "Full creative control of the company. Full Adobe Suite competence. Start-to-finish design projects. Budget setting and cash flow. Club and event creation. Promotion and marketing.",
     },
@@ -88,19 +87,7 @@ export default function Experience() {
       <h1 className="flex justify-center">Experience</h1>
 
       <Accordion data={jobs} />
-      <div className={styles.cardContainer}>
-        {jobs.map((job, i) => {
-          return (
-            <Card
-              content={job}
-              key={i}
-              index={i}
-              onClick={handleCardClick}
-              isOpen={openPopupIndex === i}
-            />
-          );
-        })}
-      </div>
+
     </div>
   );
 }
