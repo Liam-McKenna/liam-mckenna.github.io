5278fd7608e6f20961d18472f62d868a97091fd6
Liam McKenna <liam.mckenna@outlook.ie>
2024-01-29 22:33:46 +0000

    update next and improved exp section

--- src/components/accordion/AccordionItem.js
+++ src/components/accordion/AccordionItem.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import Image from 'next/image'
 import { Collapse } from 'react-collapse'
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai"
+import { FaLocationDot } from "react-icons/fa6";
+import { CgWebsite } from "react-icons/cg";
+import { GiDuration } from "react-icons/gi";
+
+
+
 
 const AccordionItem = ({ open, toggle, item }) => {
+
+
+    const SkillTag = ({ skill }) => {
+        return (
+            <div className='inline-block bg-[#673591] text-white py-.8 px-2 rounded-full opacity-70'>
+                {skill}
+            </div>
+        )
+    }
+
     return (
         <div className='pt-[10px]'>
 
@@ -16,8 +33,26 @@ const AccordionItem = ({ open, toggle, item }) => {
 
             <Collapse isOpened={open}>
                 <div className="bg-[#241D41] rounded-md mt-2 p-6 flex items-center">
-                    <p className=' flex-wrap '>{item.description}</p>
-                    <div className="w-[150px] min-w-[150px] h-[150px] rounded-full bg-gradient-to-r from-green-400 to-blue-500"></div>
+                    <div className="text-container">
+                        <div className="flex gap-5 pb-5">
+                            <div className="location flex items-center gap-2">
+                                <FaLocationDot color='#BB77FF' />
+                                {item.location}</div>
+                            <div className="website flex items-center gap-2">
+                                <CgWebsite color='#BB77FF' />
+                                {item.website}</div>
+                            <div className="duration flex items-center gap-2">
+                                <GiDuration color='#BB77FF' />
+                                <div className="duration">{item.duration}</div>
+
+                            </div>
+                        </div>
+                        <p className=' flex-wrap '>{item.description}</p>
+                        <div className="skills pt-5 flex gap-2">{item.skills.map((skill, index) => { return <SkillTag key={index} skill={skill} /> })}</div>
+                    </div>
+                    <div className="w-[150px] min-w-[150px] h-[150px] rounded-full bg-gradient-to-r from-green-400 to-blue-500">Logo
+                        <Image src="/images/NuritasLogo.png" alt="Description of image" width={500} height={500} />
+                    </div>
                 </div>
             </Collapse >
 
--- src/components/sections/Experience.js
+++ src/components/sections/Experience.js
@@ -34,51 +34,79 @@ export default function Experience() {
     {
       title: "Software Engineer",
       company: "Nuritas",
-      time: "(2yrs) Oct 2021 - Oct 2023",
+      time: "Oct 2021 - Oct 2023",
+      duration: '2 yrs',
+      location: "Dublin",
+      website: 'Nuritas.com',
       description:
         "Developed and maintained an internal data warehouse and user interface Collaborated with Data Science teams to build APIs and logic functions Championed UI/UX design, improving and implementing new designs into React",
+      skills: ["JavaScript", "Python", "React", "Django", "MySql"],
     },
     {
       title: "Web Developer",
       company: "Freelance",
-      time: "(6mos) May 2021 - Oct 2021",
+      time: "May 2021 - Oct 2021",
+      duration: '6 mos',
+      location: "Dublin",
+      website: 'NA',
       description:
         "Working directly with Clients to design and develope full stack website aplications. Focus on the MERN stack with React frontend and Node/Express backend. Data bases focused on SQL or MongoDB. Designwork in photoshop and Figma",
+      skills: ["JavaScript", "Python", "React", "Django", "MySql"],
     },
     {
       title: "Web Support Intern",
       company: "Glowfox",
-      time: "(7mos) Aug 2020 - Feb 2021",
+      time: "Aug 2020 - Feb 2021",
+      duration: '7 mos',
+      location: "Dublin",
+      website: 'Glowfox.com',
       description:
         "Worked Closely with B2B the integration of the Glofox Web plaform into the clients dedicated websites. First line of support of all web Developement quieres both internally and externally.",
+      skills: ["JavaScript", "Python", "React", "Django", "MySql"],
     },
     {
       title: "Software Developer Intern",
       company: "Cloudtech Ltd",
-      time: "(8mos) Jan 2020 - Aug 2020",
+      time: "Jan 2020 - Aug 2020",
+      duration: '8 mos',
+      location: "Kildare",
+      website: 'Cloudtech.ie',
       description:
         "Custom Software Solutions. CRM Setup & Management. Native & Web development. Project Management & Documentation.",
+      skills: ["JavaScript", "Python", "React", "Django", "MySql"],
     },
     {
       title: "1st & 2nd Tech Support",
       company: "Hewlett Packard",
-      time: "(1yr 8mos) Jun 2018 -> Jan 2020",
+      time: "Jun 2018 - Jan 2020",
+      duration: '1 yr 8 mos',
+      location: "Dublin",
+      website: 'hpe.com/ie/en/home.html',
       description:
         "Specialist IT support to first level agents. resolving major issues with clients software and hardware. Active Directory provisioning. Coaching & mentorship of peers. Control of Knowledge Base.",
+      skills: ["JavaScript", "Python", "React", "Django", "MySql"],
     },
     {
       title: "Audio Visual Technician",
       company: "Sonics AVI",
-      time: "(5mos) Aug 2018 - Jun 2018",
+      time: "Aug 2018 - Jun 2018",
+      duration: '5 mos',
+      location: "Dublin",
+      website: 'https://avispl.co.uk/locations/dublin/',
       description:
         "Provide IT support to engineers. First line of IT support to client. Building comms racks. Hardware install & setup. face to face client support. Cable crimping & running.",
+      skills: ["JavaScript", "Python", "React", "Django", "MySql"],
     },
     {
       title: "Graphic Designer & Media Manager",
       company: "Sin Nightclub",
-      time: "(5yrs 4mos) May 2012 - Aug 2017",
+      time: "May 2012 - Aug 2017",
+      duration: '5 yrs 4 mos',
+      location: "Dublin",
+      website: 'facebook.com/DublinSin/',
       description:
         "Full creative control of the company. Full Adobe Suite competence. Start-to-finish design projects. Budget setting and cash flow. Club and event creation. Promotion and marketing.",
+      skills: ["JavaScript", "Python", "React", "Django", "MySql"],
     },
   ];
 
