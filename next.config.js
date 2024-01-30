be6bfe0ef299b01f896cc20cb0d7b3de8947c615
Liam McKenna <liam.mckenna@outlook.ie>
2024-01-30 15:27:45 +0000

    disable  image optimiser

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  images: {
+    unoptimized: true
+  },
   // Optional: Add a trailing slash to all paths `/about` -> `/about/`
   // trailingSlash: true,
   // Optional: Change the output directory `out` -> `dist`
