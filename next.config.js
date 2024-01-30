cf3b1d6a4bdc0d33fe3bec8924f0bb21c33a7e4d
Liam McKenna <liam.mckenna@outlook.ie>
2024-01-30 13:25:03 +0000

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: {
-    domains: ['liam-mckenna.github.io'], // Add your own domain here
-    ...(process.env.NODE_ENV === 'development' && { unoptimized: true }),
-  },
+  // output: "export",
   // Optional: Add a trailing slash to all paths `/about` -> `/about/`
   // trailingSlash: true,
   // Optional: Change the output directory `out` -> `dist`
