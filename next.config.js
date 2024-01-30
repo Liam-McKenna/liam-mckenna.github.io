6581bed0db74524c6b7fcbce6129be89e900514a
Liam McKenna <liam.mckenna@outlook.ie>
2024-01-30 13:13:02 +0000

    added image fix for dev mode

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  images: {
+    domains: ['liam-mckenna.github.io'], // Add your own domain here
+    ...(process.env.NODE_ENV === 'development' && { unoptimized: true }),
+  },
   // Optional: Add a trailing slash to all paths `/about` -> `/about/`
   // trailingSlash: true,
   // Optional: Change the output directory `out` -> `dist`
